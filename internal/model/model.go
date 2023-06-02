f7704f65068a909479c8de24b7b93aaeac62e305
lingang <qq1812159202@163.com>
2023-06-02 15:31:11 +0800

    数据库连接

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,13 @@
 package model
 
+import (
+	"fmt"
+	"gin_blog/pkg/setting"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
 type Model struct {
 	ID        uint32 `gorm:"primary_key" json:"id"`
 	CreatedOn uint32 `json:"created_on"`
@@ -8,4 +16,27 @@ type Model struct {
 	UpdatedBy string `json:"updated_by"`
 	DeletedOn uint32 `json:"deleted_on"`
 	IsDel     uint8  `json:"is_del"`
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local",
+		databaseSetting.Username,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime)
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, err
+	}
+	sqDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	sqDB.SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	sqDB.SetMaxOpenConns(databaseSetting.MaxOpenConns)
+	return db, nil
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gin_blog/global"
+	"gin_blog/internal/model"
 	"gin_blog/internal/routers"
 	"gin_blog/pkg/setting"
 	"github.com/gin-gonic/gin"
@@ -14,6 +15,10 @@ func init() {
 	err := setupSetting()
 	if err != nil {
 		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	err = setupEngine()
+	if err != nil {
+		log.Fatalf("init.setupEngine err: %v", err)
 	}
 }
 
@@ -50,5 +55,14 @@ func setupSetting() error {
 	}
 	global.ServerSetting.ReadTimeout *= time.Second
 	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
 	return nil
 }
